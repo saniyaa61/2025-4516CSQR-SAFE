7c65f0c33aa85f48d7a7ec35f2b5d335883cbe54
Abdullah <102337@oryx.edu.qa>
2025-03-12 22:34:13 +0300

    created tkinter window and list release years

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk
 
+# adding the csv file
 df = pd.read_csv("Netflix_Dataset.csv")
+
+# list of years movies/tv shows have been released
+release_years = sorted(df["release_year"].unique())
+
+window = tk.Tk()  # created tkinter window
+window.title("Movie/tv show Ratings visualizer")  # changed title of window
+window.geometry("500x500")  # changed size of window
