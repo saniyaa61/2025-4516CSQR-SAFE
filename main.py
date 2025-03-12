a2dfa3c1e08a03ddc3c7a7abfe20ba236db45eb4
Abdullah <102337@oryx.edu.qa>
2025-03-12 22:40:05 +0300

    added boolean variables

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ release_years = sorted(df["release_year"].unique())
 window = tk.Tk()  # created tkinter window
 window.title("Movie/tv show Ratings visualizer")  # changed title of window
 window.geometry("500x500")  # changed size of window
+
+# boolean variables to keep track of the checkboxes are checked or not
+showLegend = tk.BooleanVar()
+showPercentageValues = tk.BooleanVar()
+showLabels = tk.BooleanVar()
+
+# setting them to false (making the checkboxes unchecked)
+showLegend.set(False)
+showPercentageValues.set(False)
+showLabels.set(False)
