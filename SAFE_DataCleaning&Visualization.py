e89cc93852fcf7b1057313205eaade55ea4bcf2c
saniyabharde <120030335+saniyaa61@users.noreply.github.com>
2025-03-22 15:57:05 +0000

    Dropping Unnecessary Columns from the Dataset

--- SAFE_DataCleaning&Visualization.py
+++ SAFE_DataCleaning&Visualization.py
@@ -1,32 +1,47 @@
 import pandas as pd
 
 df = pd.read_csv("Netflix Dataset.csv")  # Reading the Dataset csv file
-print("------First Row of the Netflix Dataset------")
-print("\n", df.head(1))  # Returns the first row of the Dataset
+print("---First Row of the Netflix Dataset---")
+print(df.head(1))  # Returns the first row of the Dataset
 
 print("\n", df.shape)  # Returns the no. of rows and no. of columns present in the dataset
 rows, cols = df.shape
-print("Number of Rows in the Dataset are:", rows)
-print("Number of Columns in the Dataset are:", cols)
+print("Number of Rows in the Dataset are:", rows)  # Returns only the no. of rows present in the dataset
+print("Number of Columns in the Dataset are:", cols)  # Returns only the no. of columns present in the dataset
 
-print("------Information of the Netflix Dataset------")
-print("\n", df.info())  # Returns all column names, non-null values, count, and thier datatypes
+print("\n---Information of the Netflix Dataset---")
+print(df.info())  # Returns all column names, non-null values, count, and thier datatypes
 
-print("------Description of the Netflix Dataset------")
-print("\n", df.describe())  # Returns arithemic calculations based on all numeric data found in the dataset
+print("\n---Description of the Netflix Dataset---")
+print(df.describe())  # Returns arithemic calculations based on all numeric data found in the dataset
 
-print("------Checking for Missing Values / Empty Rows in the Netflix Dataset------")
-print("\n", df.isnull().sum())  # Checks for null values and then returns their sum
+print("\n---Checking for Missing Values / Empty Rows in the Netflix Dataset---")
+print(df.isnull().sum())  # Checks for null values and then returns their sum
 
 df.dropna(inplace=True)  # Deletes / Drops all rows containing null values
-print("------Checking if the Missing Values / Empty Rows in the Netflix Dataset are dropped------")
-print("\n", df.isnull().sum())  # Checks for null values and then returns their sum
+print("\n---Checking if the Missing Values / Empty Rows in the Netflix Dataset are dropped---")
+print(df.isnull().sum())  # Checks for null values and then returns their sum
 
-print("------Updated Information of the Netflix Dataset------")
-print("\n", df.info())
+print("\n---Updated Information of the Netflix Dataset---")
+print(df.info())  # Returns all column names, non-null values, count, and thier datatypes
 
-print("------Updated Shape of the Netflix Dataset------")
-print("\n", df.shape)
+print("\n---Updated Shape of the Netflix Dataset---")
+print(df.shape)  # Returns the no. of rows and no. of columns present in the dataset
 rows, cols = df.shape
-print("Number of Rows in the Dataset after filtering are:", rows)
-print("Number of Columns in the Dataset after filtering are:", cols)
+print("Number of Rows in the Dataset after filtering are:", rows)  # Returns only the no. of rows present in the dataset
+print("Number of Columns in the Dataset after filtering are:", cols)  # Returns only the no. of columns present in the dataset
+
+print("\n---Different Columns of the Netflix Dataset---")
+print(df.columns)  # Returns all the columns present in the dataset
+
+cols = ['show_id', 'description']  # Making a list of two columns present in the dataset
+df.drop(columns=cols, inplace=True)  # Deletes / drops the list cols containing 'show_id' and 'description' columns
+df.reset_index(drop=True, inplace=True)  # Resets the index of the dataset
+
+print("\n---Updated Columns of the Netflix Dataset---")
+print(df.columns) # Returns all the columns present in the dataset
+
+print("\n---Updated Netflix Dataset with 10 Columns---")
+print(df.head())  # Returns the first 5 rows of the dataset by default
+
+
